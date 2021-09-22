9ca99fa7590c6ad6336121948c989b54a9768fde
Ksardas1 <ksardas00@gmail.com>
2021-09-22 21:19:47 +0300

    complete two

--- three.dart
+++ three.dart
@@ -4,7 +4,21 @@ import 'dart:io';
 // Строковые значения должны игнорироваться с соответствующим сообщением
 
 void main() {
-  print('Input: ');
-  int? n = int.parse(stdin.readLineSync()!);
-  print(n);
+  List<int> numberList;
+  for (;;) {
+    // Запуск бесконечного цикла
+    print('Введите целое число: '); // Запрос ввода
+    String? name = stdin.readLineSync(); // Ввод
+    final check = int.tryParse(name!); // Попытка присвоения строки в виде числа
+
+    if (check == null) {
+      // Если не получилось
+      print('Это не целое число'); // Говорим, что это не число
+    } else {
+      // Если получилось
+      numberList.add(check);
+      print(check); // Выводим на экран
+      print('' + numberList.length);
+    }
+  }
 }
--- two.dart
+++ two.dart
@@ -4,7 +4,18 @@ import 'dart:io';
 // При вводе строки выдавать сообщение, что ввод был не в виде числа
 
 void main() {
-  print('Input: ');
-  int? n = int.parse(stdin.readLineSync()!);
-  print(n);
+  for (;;) {
+    // Запуск бесконечного цикла
+    print('Введите целое число: '); // Запрос ввода
+    String? name = stdin.readLineSync(); // Ввод
+    final check = int.tryParse(name!); // Попытка присвоения строки в виде числа
+
+    if (check == null) {
+      // Если не получилось
+      print('Это не целое число'); // Говорим, что это не число
+    } else {
+      // Если получилось
+      print(check); // Выводим на экран
+    }
+  }
 }
